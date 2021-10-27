1e225429c1d044756f7aee6786d091be7aef9847
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2021-10-27 02:15:52 -0300

    feat: handling errors and fixing documentation

--- app/main.py
+++ app/main.py
@@ -1,18 +1,23 @@
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from app.model import DenavitHartenberg
 from app.utils import multiply_matrix
+from app.docs import RESPONSES_EXAMPLE
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/', responses={200: RESPONSES_EXAMPLE.get('dh')})
 def dh():
-    return {"dh": "Denavit-Hartenberg API"}
+    return {'dh': 'Denavit-Hartenberg API'}
 
 
-@app.post("/calculate-dh/")
+@app.post('/calculate-dh/', responses={200: RESPONSES_EXAMPLE.get('calculate_dh')})
 async def calculate_dh(dh_list: List[DenavitHartenberg]):
-    dh_calc, x, y, z = multiply_matrix(dh_list)
-    return {"result": dh_calc, "coord": {"x": x, "y": y, "z": z}}
+    dh_calc, x, y, z, error = multiply_matrix(dh_list)
+
+    if error:
+        raise HTTPException(status_code=404, detail=error)
+
+    return {'result': dh_calc, 'coord': {'x': x, 'y': y, 'z': z}}
--- app/utils.py
+++ app/utils.py
@@ -4,6 +4,13 @@ from typing import List, Tuple
 from app.model import DenavitHartenberg
 
 
+def linalg_multi_dot(dh: List[np.ndarray]) -> np.ndarray:
+    if len(dh) > 1:
+        return np.linalg.multi_dot(dh)
+
+    return dh[0]
+
+
 def get_coord(dh: np.ndarray) -> Tuple:
     vector = np.array([[0], [0], [0], [1]])
     x, y, z, _ = dh.dot(vector).tolist()
@@ -26,7 +33,11 @@ def generate_dh(dh: DenavitHartenberg) -> np.ndarray:
 
 
 def multiply_matrix(matrices: List) -> Tuple:
-    dh_matrices = [generate_dh(dh=m) for m in matrices]
-    dh_array = np.linalg.multi_dot(dh_matrices)
-    x, y, z = get_coord(dh=dh_array)
-    return dh_array.tolist(), x, y, z
+    try:
+        dh_matrices = [generate_dh(dh=m) for m in matrices]
+        dh_array = linalg_multi_dot(dh=dh_matrices)
+        x, y, z = get_coord(dh=dh_array)
+    except ValueError as e:
+        return [], 0, 0, 0, str(e)
+
+    return dh_array.tolist(), x, y, z, None
